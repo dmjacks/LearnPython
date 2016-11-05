92831cbacf243131dfb0782d9a187bd9e2534063
dmjacks <d.m.jackson44@gmail.com>
2016-11-05 10:46:01 -0700

    Add files via upload

---
+++ ex7.py
@@ -0,0 +1,53 @@
+# prints the following sentence
+print "Mary had a little lamb."
+
+# prints the following sentence
+print "Its fleece was white as %s." % 'snow'
+
+# prints the following sentence
+print "And everywhere that Mary went."
+
+
+print "." * 10 #this prints the period 10 times
+
+# sets variable end1 equal to the string c
+end1 = "c"
+
+# sets variable end2 equal to the string h
+end2 = "h"
+
+# sets variable end3 equal to the string e
+end3 = "e"
+
+# sets variable end4 equal to the string e
+end4 = "e"
+
+# sets variable end5 equal to the string s
+end5 = "s"
+
+# sets variable end6 equal to the string e
+end6 = "e"
+
+# sets variable end7 equal to the string B
+end7 = "B"
+
+# sets variable end8 equal to the string u
+end8 = "u"
+
+# sets variable end9 equal to the string r
+end9 = "r"
+
+# sets variable end10 equal to the string g
+end10 = "g"
+
+# sets variable end11 equal to the string e
+end11 = "e"
+
+# sets variable end12 equal to the string r
+end12 = "r"
+
+# The comma allows the following print statement to be 
+#printed on the same line
+
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
